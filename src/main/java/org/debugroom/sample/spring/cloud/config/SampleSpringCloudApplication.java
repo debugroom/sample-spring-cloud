45b461bfc9c31af3d3c5ca8f5a652e07307e5458
kohei.kawabata <kohei.kawabata@mac.com>
2018-07-13 18:52:11 +0900

    Create initial spring boot project.

---
+++ src/main/java/org/debugroom/sample/spring/cloud/config/SampleSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package org.debugroom.sample.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringCloudApplication.class, args);
+    }
+}
